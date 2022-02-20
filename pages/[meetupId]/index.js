415765cc5510b81ae5fc36c9eb20032874bac4f2
WY K <ablack1029@gmail.com>
2022-02-21 01:37:30 +0900

    Add : getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,3 +11,41 @@ export default function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    //fallback-paths 배열이 모든 지원되는 매개변수를 저장할지, 일부만 저장할지 설정
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      }, //dynamic page당  객체 하나
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      mettupData: {
+        image:
+          "https://images.unsplash.com/photo-1439405326854-014607f694d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        title: "First Meetup",
+        id: meetupId,
+        address: "Some Street",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
