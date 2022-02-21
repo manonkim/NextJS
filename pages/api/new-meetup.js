e74865486b73b7e8fe7a7928846d62399d07f4de
WY K <ablack1029@gmail.com>
2022-02-21 17:24:58 +0900

    Add : db로 data 보내기

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -3,27 +3,27 @@ import { MongoClient } from "mongodb";
 // /api/net-meetup
 // only POST /api/net-meetup
 
-async export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const client =  await MongoClient.connect(
-      'mongodb+srv://abd:ehM3Cr1mwMbDoWg3@cluster0.6baoc.mongodb.net/meetups?retryWrites=true&w=majority'
+    const client = await MongoClient.connect(
+      "mongodb+srv://abd:ehM3Cr1mwMbDoWg3@cluster0.6baoc.mongodb.net/meetups?retryWrites=true&w=majority"
     );
-    const db = client.db()
+    const db = client.db();
 
     //collection은 SQL DB에 있는 tables이고 문서는 해당 tables의 항목
     //단일 meetup은 하나의 문서 > collection = 여러 문서(meetup)를 보관
-    const meetupsCollection = db.collection('meetups');  //collection name 아무거나 지정가능
+    const meetupsCollection = db.collection("meetups"); //collection name 아무거나 지정가능
 
     //컬렉션에 새 문서를 삽입하기위해 query명령 중 하나인 insertOne 호출
     //mongodb는 문서가 JS의 obejct
-    const result = await meetupsCollection.insertOne({data});
+    const result = await meetupsCollection.insertOne(data);
 
     console.log(result);
     //입력 후 db 연결 차단
     client.close();
 
-    res.status(201).json({message : 'Meetup inserted!'})
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,26 @@
 //our-domain.com/new-meetup
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+  //데이터를 수집해서 db로 보내는 함수
+  //fetch function이나 axios 등을 사용하여 보낸다.
+  async function addMeetupHandler(enteredMeetupData) {
+    //api폴더/폴더이름  파일로가서 함수를 trigger
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      //body에 내용을 입력데이터 넣어줌
+      headers: {
+        "Content-Type": "application/json",
+      },
+      //JSON 데이터 보내는 것을 명확하게 하기위해 header추가
+    });
+    const data = await response.json();
+    console.log(data);
+
+    router.push("/");
   }
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 }
